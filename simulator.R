1bdde64ed160bfcc82c712175222040cf189f01e
Matt Waldeck <matt@Matts-MacBook-Pro.local>
2021-04-08 00:31:08 -0400

    completed base framework for simulator and removed needless files

--- simulator.R
+++ simulator.R
@@ -2,7 +2,8 @@
 #function will examine model inputs and determine whether to buy or sell shares on a given day 
 
 train.model <- function(data){
-  model <- runif(n = 1, min = 0, max = 1)
+
+  model <- runif(n = 1, min = .75, max = 1)
   return(model)
 }
 model.predict <- function(model, predictors){
@@ -11,7 +12,7 @@ model.predict <- function(model, predictors){
 }
 
 decision <- function(model, predictors){
-  if (model.predict(model,predictors) > .5){
+  if (as.double(model.predict(model,predictors)) > .5){
     return(TRUE)
   }
   else {
@@ -19,20 +20,46 @@ decision <- function(model, predictors){
   }
 }
 
+#current implementation allows debt
+begin.day <- function(predictors, model, capital){
+  shares <- 0
+   if (decision(model, predictors)){
+   shares <- 1
+   capital <- as.double(capital - (predictors["Low"]))
+   } 
+  output <- c(capital, shares)
+}
 
+end.day <- function(shares, capital, closing.price){
+  if (shares > 0) {
+    return <- closing.price * shares
+    capital <- capital + return
+  }
+  return(capital)
+}
 
 simulate <- function(data, model, capital){
   
-  df <- read_csv(data)
-  for(i in 1:nrow(df)){
-    predictors <- df[i,]
-    if (model.predict(model, predictors))
-  } 
+  current.shares <- 0
   
-}
+  
+  for(i in 1:nrow(data)){
+    predictors <- data[i,]
+    #begin day
+    update <- begin.day(predictors, model, capital)
+    capital <- as.double(update[1])
+    shares <- as.integer(update[2])
+    #end day
+    capital <- as.double(end.day(shares, capital, predictors["High"]))
+  }
+  return(capital)
+ }
 
 
 main <- function(){
+  data <- read_csv("AMZNtrain.csv")
+  model <- train.model(data)
+  simulate(data, model, 10000)
   
   
 }
